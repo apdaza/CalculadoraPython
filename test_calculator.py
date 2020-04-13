f0a6d18d8faee508c2e4888450a10263b3c728da
apdaza <apdaza@gmail.com>
2020-04-13 14:58:47 -0500

    implementada la suma

--- test_calculator.py
+++ test_calculator.py
@@ -8,9 +8,9 @@ from calculadora import *
 class TestCalculadora:
 
     def test_suma(self):
-        this.calculadora = Calculadora()
-        assert 4 == this.calculadora.sumar(2, 2)
+        calculadora = Calculadora()
+        assert 4 == calculadora.sumar(2, 2)
 
     def test_resta(self):
-        this.calculadora = Calculadora()
-        assert 2 == this.calculadora.restar(4, 2)
+        calculadora = Calculadora()
+        assert 2 == calculadora.restar(4, 2)
